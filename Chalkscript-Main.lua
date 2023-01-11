c06bcf80638636ef0fc09a8ad8286b40de2666a7
Гадюка <65050517+ViperOne1@users.noreply.github.com>
2023-01-10 23:51:34 -0500

    Update Chalkscript-Main.lua

--- Chalkscript-Main.lua
+++ Chalkscript-Main.lua
@@ -1,11 +1,11 @@
-async_http.init('raw.githubusercontent.com','/ViperOne1/Chalkscript/main/raw/Chalkscript-Raw.lua',function(a)
-    local err = select(2,load(a))
+async_http.init('raw.githubusercontent.com','/ViperOne1/Chalkscript/main/raw/Chalkscript-Raw.lua',function(contents)
+    local err = select(2,load(contents))
     if err then
         util.toast("-Chalkscript-\n\nScript Failed to Download off Github.")
     return end
-    local f = io.open(filesystem.scripts_dir()..SCRIPT_RELPATH, "wb")
-    f:write(a)
-    f:close()
+    local csLua = io.open(filesystem.scripts_dir()..SCRIPT_RELPATH, "wb")
+    csLua:write(contents)
+    csLua:close()
     util.toast("-Chalkscript\n\nSuccessfully Installed Chalkscript.\nHave Fun!")
     util.restart_script()
 end)
